612bc50f7fa55ca8e4fbbdbb536377685654de1a
Samar Chandra <samarc476@gmail.com>
2023-12-22 13:13:07 +0530

    Initial Commit

---
+++ WeatherAppV1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherAppV1
+//
+//  Created by Samar Chandra on 22/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherAppV1/WeatherAppV1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppV1App.swift
+//  WeatherAppV1
+//
+//  Created by Samar Chandra on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
